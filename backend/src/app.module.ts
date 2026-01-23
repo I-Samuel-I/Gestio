3ee83cc0f2b3e83676975b64bfe97ddcc6ac2ac8
graziclaus <graziclaus082@gmail.com>
2026-01-23 16:24:09 -0300

    chore: remove accidental root-level node setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
     imports: [
@@ -21,6 +22,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         autoLoadEntities: true,
         synchronize: true, 
         }),
+
+        AuthModule,
     ],
     controllers: [AppController],
     providers: [AppService],
