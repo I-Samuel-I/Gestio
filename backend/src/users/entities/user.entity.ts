2566f4bb5eba985656b888d2f22a066cec2ce63d
graziclaus <graziclaus082@gmail.com>
2026-02-11 21:03:16 -0300

    fix(users): adjust users module with roles, status, and contact info

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,10 +1,30 @@
 import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm';
 
+export enum UserRole {
+    
+    ADMIN = 'admin',
+    SUPERVISOR = 'supervisor',
+    SELLER = 'seller',
+    SUPPORT = 'support',
+    FINANCIAL = 'financial',
+}
+
+export enum UserStatus {
+
+    ACTIVE = 'active',
+    INACTIVE = 'inactive',
+    PENDING = 'pending'
+
+}
+
 @Entity('users')
 export class User {
 
     @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
+    name: string;
 
     @Column({ unique: true })
     email: string;
@@ -13,13 +33,24 @@ export class User {
     password: string;
 
     @Column({
-
         type: 'enum',
-        enum: ['admin', 'user'],
-        default: 'user',
-        
+        enum: UserRole,
+        default: UserRole.SELLER
     })
-    role: 'admin' | 'user';
+    role: UserRole
+
+    @Column({
+        type: 'enum',
+        enum: UserStatus,
+        default: UserStatus.PENDING,
+    })
+    status: UserStatus;
+
+    @Column({nullable:true})
+    company?: string;
+
+    @Column({nullable:true})
+    phone?: string;
 
     @Column({ default: true })
     isActive: boolean;
