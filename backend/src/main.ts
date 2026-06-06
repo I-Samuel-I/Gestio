fcc5f60883a8974f3f1433a33aa5f203ad3e5df1
graziclaus <graziclaus082@gmail.com>
2026-06-06 16:37:03 -0300

    feat(backend): add dependencies

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -15,6 +16,15 @@ async function bootstrap() {
             transform: true,
         }),
     );
+
+    const config = new DocumentBuilder()
+        .setTitle('Gestio API')
+        .setDescription('Documentação da API do Gestio')
+        .setVersion('1.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
 
     await app.listen(process.env.PORT ?? 3000);
 }
